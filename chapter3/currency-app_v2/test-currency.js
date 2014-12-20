bc0e7a60d124d4df9ac1779201aaa6c78339a744
Glenn Castro <glenn.castro@gmail.com>
2014-12-20 23:18:05 +1100

    Chapter 3 - currency-app version 2

--- chapter3/currency-app_v2/test-currency.js
+++ chapter3/currency-app_v2/test-currency.js
@@ -2,4 +2,6 @@ var Currency = require('./currency');
 var canadianDollar = 0.91;
 
 var currency = new Currency(canadianDollar);
-console.log(currency.canadianToUS(50));
+console.log("50 Canadian to US $: " + currency.canadianToUS(50));
+
+console.log("50 US to Canadian $: " + currency.USToCanadian(50));
