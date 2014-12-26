6502fc74bea0b4d7dab978e16998211c362efb23
Glenn Castro <glenn.castro@gmail.com>
2014-12-26 23:45:51 +1100

    Chapter 5 - NoSQL - Redis pub/sub example

--- chapter5/nosql/redis/chatserver_redis.js
+++ chapter5/nosql/redis/chatserver_redis.js
@@ -5,16 +5,15 @@ var server = net.createServer(function(socket) {
 	var subscriber;
 	var publisher;
 
-	socket.on('connect', function() {
-		subscriber = redis.createClient();
-		subscriber.subscribe('main_chat_room');
+	console.log('client connected');
+	subscriber = redis.createClient();
+	subscriber.subscribe('main_chat_room');
 
-		subscriber.on('message', function(channel, message) {
-			socket.write('Channel ' + channel + ': ' + message);
-		});
-
-		publisher = redis.createClient();
+	subscriber.on('message', function(channel, message) {
+		socket.write('Channel ' + channel + ': ' + message);
 	});
+
+	publisher = redis.createClient();
 
 	socket.on('data', function(data) {
 		publisher.publish('main_chat_room', data);
